1dfe45001b7e02630e5cafe3712cdbacbba98d44
Umang <umangborad123@gmail.com>
2017-08-22 11:26:59 -0400

    Final commit

--- test1.js
+++ test1.js
@@ -1,12 +1,51 @@
 /**
  * Test 1
- * This is the first coding challenge. Below are two arrays. 
+ * This is the first coding challenge. Below are two arrays.
  * The second array is identical to the first except for one number.
  * Find the missing number and log it in the console
  *
- * You may use any development tools or package manager you like. 
+ * You may use any development tools or package manager you like.
  * Lodash is included as a bower.json dependency in case you would like to use it.
  */
 
 var arr = [1,2,5,7,4,8,12,15,17,37];
 var secondArr = [1,17,5,12,4,7,15,2,37];
+
+// Different ways to solve this Problem.
+//
+// 1) We can sort both arrays and then check when there is a mismatch
+//    between the two arrays.
+//
+//    OR
+//
+// 2) No need to sort both arrays and just check when there is a mismatch.
+
+
+function findMissingNumber (first, second) {
+
+  var sortedFirst = first.sort(function (a, b) {
+    return a-b;
+  });
+
+  var sortedSecond = second.sort(function (a, b) {
+    return a-b;
+  });
+
+  var missing; // Assuming we have only one missing value.
+              // If we have more than one then we can use array here.
+
+  // We can define a function like Array.prototype.missing = function(sortedSecond) {}
+  // and write logic there and call it using sortedFirst.missing(sortedFirst)
+  // or we can do it as follows:
+
+  for (var i in sortedFirst) {
+    if (sortedSecond.indexOf(sortedFirst[i]) < 0) {
+      missing = first[i];
+      break;
+    }
+  }
+
+  console.log("Solution for TEST 1 ---> Number missing in second array is", missing);
+}
+
+findMissingNumber(arr, secondArr);
--- test2.js
+++ test2.js
@@ -8,34 +8,53 @@
  */
 
 var count = 4;
-function foo() {
+
+function foo(count) {
   var table = [count];
-  while (count-- > 0) {
-    table.push([]);
-  }
-  for (var count = 1; count < table.length; count++) {
-    table[count].push(count);
+
+  // No need for this while loop to get the desired output.
+
+  // while (count-- > 0) {
+  //   table.push([]);
+  // }
+
+  // We cannot use table[count].push here, instead use table.push
+  // Here if we use count < table.length then it will go to infinity loop.
+
+  for (var i = 1; i <= count; i++) {
+    table.push([i]);
   }
   return table;
 }
 
-// console.log(foo());
+console.log("Solution for TEST 2 (a)", foo(count));
 
 ////////
 // OR //
 ////////
 
 /*
- * The desired result is a countdown from 5 to 0 using console log messages. 
+ * The desired result is a countdown from 5 to 0 using console log messages.
  * Explain why the code only logs -1, then fix the code so it works as expected.
  */
 
+ // Problem:
+ // Here, setTimeout function will be stored in a queue 6 times and when i becomes 6 it will execute
+ // console.log(num - i) 6 times which will be 5 - 6 = -1.
 function countdown (num) {
+  // Two ways to solve this problem.
+  // 1) Define i using let instead of var.
+  // 2) use IIFE (Immediately Invoked Function Expression) inside for loop and
+  //    passing the reference of i as an argument to an iife function.
+  //    When we use iife function, correct value of i is stored and then it works perfectly.
   for (var i = 0; i <= num; i += 1) {
-    setTimeout(function () {
-      console.log(num - i);
-    }, i * 1000);
+
+    (function iife(i){
+      setTimeout(function () {
+        console.log(num - i);
+      }, i * 1000);
+    })(i);
   }
 }
 
-// countdown(5);
+console.log("Solution for TEST 2 (b)",countdown(5));
